7c84bb6766f6c59d7529c052c491c947c129220f
Prajwalahc <89767600+Prajwalahc@users.noreply.github.com>
2021-09-16 04:32:25 -1200

    Create Activity.h

---
+++ Activity.h
@@ -0,0 +1,9 @@
+#ifndef ACTIVITY3_H_INCLUDED
+#define ACTIVITY3_H_INCLUDED
+#include<avr/io.h>
+#include "Activity2.h"
+
+char OutPWM(uint16_t temp_value);
+void InitPWM(void);
+
+#endif // ACTIVITY3_H_INCLUDED
